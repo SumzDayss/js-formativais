49764be295e2f5f1167116aad7a864385dd2de7e
Yagon Don <you@example.com>
2025-10-20 09:29:24 +0300

    Added 8.uzd, half working.Car thing script

---
+++ objects.js
@@ -0,0 +1,15 @@
+
+
+function brand(x) {
+  return x ;
+}
+carYear = 2016;
+const car = ["Toyota","Civic","2016"]
+
+console.log(car[0]);
+
+if (car <= 2015){
+    console.log("Māšīna ir veecāka par 10 gaidem")
+} else {
+    console.log("Auto nav vecāks par 10 gadiem")
+}
