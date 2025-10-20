abf4843d6373a965b70b98bc86e1139cea48699d
Yagon Don <you@example.com>
2025-10-20 09:19:39 +0300

    Added 7.uzd, Not working

---
+++ function.js
@@ -0,0 +1,8 @@
+function intSequence(first, last) {
+
+    console.log(intSequence[1,8]);
+    console.log(intSequence[1,10]);
+    console.log(intSequence[7,16]);
+    for (i = first; i < last;i++);
+    console.log(intSequence[i])
+    }
